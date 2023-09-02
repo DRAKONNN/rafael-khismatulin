142b5f7848cba996765ec2c191a117a89578f18b
DRAKONNN <rafakhis14@gmail.com>
2023-09-02 14:54:57 +0200

    Developing experience react

---
+++ src/dataexperience.js
@@ -0,0 +1,29 @@
+export default [{
+  id: 'ayuntamiento04', 
+  title: 'Técnico Webmaster',
+  company: 'Ayuntamiento de Pedrezuela',
+  description: 'Desarrollo y actualización de la nueva página web oficial del Ayuntamiento de Pedrezuela y otros proyectos.',
+  commencement: 'Julio 2022',
+  termination: 'Julio 2023',
+}, {
+  id: 'soporte03', 
+  title: 'Técnico de Soporte',
+  company: 'BeDataScience',
+  description: 'Soporte al equipo integrándose en el diseño de cuadro de mando, actualización y envío de información final al cliente.',
+  commencement: 'Diciembre 2021',
+  termination: 'Enero 2022',
+}, {
+  id: 'fct02', 
+  title: 'FCT Desarrollador de Aplicaciones Móviles',
+  company: 'KUBUS',
+  description: 'Desarrollador de una aplicación móvil de pedidos y APIs.',
+  commencement: 'Abril 2021',
+  termination: 'Junio 2021',
+}, {
+  id: 'violinista01', 
+  title: 'Violinista',
+  company: 'Ermitage Musical',
+  description: 'Violinista en despedidas de funerales.',
+  commencement: 'Junio 2020',
+  termination: 'Abril 2022',
+}];
