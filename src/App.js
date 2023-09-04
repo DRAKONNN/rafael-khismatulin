8dce6b2972fcbd92cea405836f04690e007f3df7
DRAKONNN <rafakhis14@gmail.com>
2023-09-04 14:46:13 +0200

    Changing icons skills

--- src/App.js
+++ src/App.js
@@ -130,20 +130,19 @@ function App() {
             <section class="resume-section" id="habilidades">
                 <div class="resume-section-content">
                     <h2 class="mb-5">Habilidades</h2>
-                    <div class="subheading mb-3">Programming Languages & Tools</div>
+                    <div class="subheading mb-3">Lenguajes de programación y herramientas</div>
                     <ul class="list-inline dev-icons">
-                        <li class="list-inline-item"><i class="fab fa-html5"></i></li>
-                        <li class="list-inline-item"><i class="fab fa-css3-alt"></i></li>
-                        <li class="list-inline-item"><i class="fab fa-js-square"></i></li>
-                        <li class="list-inline-item"><i class="fab fa-angular"></i></li>
-                        <li class="list-inline-item"><i class="fab fa-react"></i></li>
-                        <li class="list-inline-item"><i class="fab fa-node-js"></i></li>
-                        <li class="list-inline-item"><i class="fab fa-sass"></i></li>
-                        <li class="list-inline-item"><i class="fab fa-less"></i></li>
-                        <li class="list-inline-item"><i class="fab fa-wordpress"></i></li>
-                        <li class="list-inline-item"><i class="fab fa-gulp"></i></li>
-                        <li class="list-inline-item"><i class="fab fa-grunt"></i></li>
-                        <li class="list-inline-item"><i class="fab fa-npm"></i></li>
+                      <li class="list-inline-item"><i class="fab fa-java"></i></li>
+                      <li class="list-inline-item"><i class="fab fa-android"></i></li>
+                      <li class="list-inline-item"><i class="fab fa-html5"></i></li>
+                      <li class="list-inline-item"><i class="fab fa-css3-alt"></i></li>
+                      <li class="list-inline-item"><i class="fab fa-js-square"></i></li>
+                      <li class="list-inline-item"><i class="fab fa-angular"></i></li>
+                      <li class="list-inline-item"><i class="fab fa-react"></i></li>
+                      <li class="list-inline-item"><i class="fab fa-node-js"></i></li>
+                      <li class="list-inline-item"><i class="fab fa-php"></i></li>
+                      <li class="list-inline-item"><i class="fab fa-wordpress"></i></li>
+                      <li class="list-inline-item"><i class="fab fa-npm"></i></li>
                     </ul>
                     <div class="subheading mb-3">Workflow</div>
                     <ul class="fa-ul mb-0">
